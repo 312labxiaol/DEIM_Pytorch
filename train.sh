b645cbe55168a5497d87cccaeb4ab797874b23dd
312labxiaol <lx@stu.shzu.edu.cn>
2025-04-20 17:46:22 +0800

    main

--- train.sh
+++ train.sh
@@ -5,7 +5,8 @@
 
 
 # -S
-CUDA_VISIBLE_DEVICES=0 torchrun --master_port=7777 --nproc_per_node=1 train.py -c configs/deim_dfine/deim_hgnetv2_s_cotton.yml --seed=0 -t /home/adminpc/workspace/DEIM_Pytorch/weight/deim_dfine_hgnetv2_s_coco_120e.pth
+# CUDA_VISIBLE_DEVICES=0 torchrun --master_port=7777 --nproc_per_node=1 train.py -c configs/deim_dfine/deim_hgnetv2_s_cotton.yml --seed=0 -t /home/adminpc/workspace/DEIM_Pytorch/weight/deim_dfine_hgnetv2_s_coco_120e.pth
+CUDA_VISIBLE_DEVICES=0 torchrun --master_port=7777 --nproc_per_node=1 train.py -c configs/deim_dfine/dfine_hgnetv2_s_cotton.yml --seed=0 -t /home/adminpc/workspace/DEIM_Pytorch/weight/deim_dfine_hgnetv2_s_coco_120e.pth
 
 # deim_dfine_hgnetv2_s_coco_120e.pth为coco预训练权重
 # python train.py -c configs/deim_dfine/deim_hgnetv2_n_cotton.yml --seed=0 -t D:/lx/DEIM-main/weight/deim_dfine_hgnetv2_n_coco_160e.pth
